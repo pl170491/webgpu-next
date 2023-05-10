bc8a2ad775aee49fb49f207976315d100fd45ee4
Davin <lampdavin@gmail.com>
2023-05-10 08:12:50 -0400

    Update gpuInit

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,43 @@
 
 // const inter = Inter({ subsets: ['latin'] });
 
-'use client';
+"use client";
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const gpuRef = useRef(null);
+  const gpuRef = useRef<Promise<GPUDevice | null> | null>(null);
 
   useEffect(() => {
-    const gpuDevice = global.navigator.gpu;
-  });
+    const gpuInit = () => {
+      if (!navigator.gpu) {
+        gpuRef.current = null;
+        return;
+      }
+
+      const gpuDevicePromise = navigator.gpu
+        .requestAdapter()
+        .then((adapter) => {
+          if (!adapter) return null;
+          else return adapter.requestDevice();
+        })
+        .catch((reason) => {
+          console.log(reason);
+          return null;
+        });
+      gpuRef.current = gpuDevicePromise;
+
+      return async () => {
+        if (!gpuRef.current) return;
+
+        const gpuDevice = await gpuRef.current;
+        if (!gpuDevice) return;
+
+        gpuDevice.destroy();
+      };
+    };
+    gpuInit();
+  }, []);
+
   return <h1>Hello!</h1>;
 }
