b7bc3b19ba1d0e7e4bc821378c259d957f32a9ec
Davin Lampert <lampdavin@gmail.com>
2023-09-07 16:33:18 -0400

    Create Code Exposition Directory

--- app/code-exposition/page.tsx
+++ app/code-exposition/page.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
 export default function Index() {
-  return <></>;
+  return (
+    <>
+      <p>
+        Here is located the various tools and helper functions I&apos;ll be
+        developing to help in my effort to beautify and streamline code
+        exposition. It is somewhat meta to try to explain code-exposition code
+        without having the proper code-exposition tools, so the articles may
+        seem janky.
+      </p>
+      <ul>
+        <li>
+          <Link href={'code-exposition/code-diff'}>Code Diff</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
